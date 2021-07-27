5a9b8141df6e6d331aa3477f40cba2f187e84641
Pablo Pompey - Emerix <ppompey@emerix.net>
2021-07-27 00:25:19 -0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const path = require('path');
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// Serve static files
-app.use(express.static(__dirname + '/dist/Simulation'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/simulation'));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/Simulation/index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/simulation/index.html'));
 });
 
-// default Heroku port
-app.listen(process.env.PORT || 8080);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 5000);
