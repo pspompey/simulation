089f391d2fe4987776d7c73b6252950baab6e9f3
Pablo Pompey - Emerix <ppompey@emerix.net>
2021-07-26 23:28:26 -0300

    fix dist

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/simulation'));
+app.use(express.static('./dist/Simulation'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/Simulation/'}),
